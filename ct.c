9ced1a4c2e62209ea25ad9bebe6438f30aa46af2
DavidAntonySelvaraj <42674179+DavidAntonySelvaraj@users.noreply.github.com>
2018-09-28 16:36:27 +0530

    Add files via upload

--- ct.c
+++ ct.c
@@ -1,12 +1,79 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c;
-printf("type the sides of triangle");
-scanf("%d%d%d",&a,&b,&c);
-if((a+b)>c&&(b+c)>a&&(c+a)>b)
-printf("it forms a triangle");
+int a,h,c;
+char v;
+printf("T FOR TEMPO \n C FOR CAR \n B FOR BUS");
+scanf("%c",&v);
+printf("type the no. of hours");
+scanf("%d",&h);
+switch(v)
+{
+case 'C':
+{
+if(h<=2)
+{ 
+c=h*20;
+}
 else
-printf("it does not forms a triangle");
+{a=h-2;
+c=a*30+40;
+}}
+printf("the cost is %d",c);
+break;
+case 'c':
+{
+if(h<=2)
+c=h*20;
+else
+a=h-2;
+c=a*30+40;
+}
+printf("the cost is %d",c);
+break;
+case 'B':
+{
+if(h<=2)
+{
+c=h*40;
+}
+else
+{
+a=h-2;
+}
+c=a*50+80;
+}
+printf("the cost is %d",c);
+break;
+case 'b':
+{
+if(h<=2)
+c=h*40;
+else
+a=h-2;
+c=a*50+80;
+}
+printf("the cost is %d",c);
+break;
+case 'T':
+{
+if(h<=2)
+c=h*30;
+else
+a=h-2;
+c=a*40+60;
+}
+printf("the cost is %d",c);
+break;
+case 't':
+{
+if(h<=2)
+c=h*30;
+else
+a=h-2;
+c=a*40+60;
+}
+printf("the cost is %d",c);
+}
 return 0;
 }
